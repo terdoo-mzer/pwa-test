be21bc6780114b50f45b552d0c2c3c0fe9ab1fc1
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2024-11-02 12:12:53 +0100

    test alert for non chromium browsers

--- src/App.vue
+++ src/App.vue
@@ -4,59 +4,93 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 
 const deferredPrompt = ref(null);
-const installButtonVisible = ref(false);
+// const isInstallButtonVisible = ref(false);
 const outputText = ref('');
+const isChromiumBrowser = ref(false);
+const isInstalled = ref(false)
+const showPwaModalVisible = ref(false)
 
-const isFirefox = navigator.userAgent.toLowerCase().includes("firefox");
-if (isFirefox && !window.matchMedia('(display-mode: standalone)').matches) {
-    alert("For a better experience, add this app to your home screen.");
+// Platform Detection
+const isSafariMobile = /iPhone|iPad|iPod/.test(navigator.userAgent) && !!navigator.userAgent.match(/Safari/);
+const isFirefoxMobile = /Android/.test(navigator.userAgent) && /Firefox/.test(navigator.userAgent);
+const isFirefoxDesktop = !isFirefoxMobile && /Firefox/.test(navigator.userAgent);
+const isSafariDesktop = !isSafariMobile && /Safari/.test(navigator.userAgent);
+
+// Check browser type to determine if to show the default or custom PWA installation button
+const verifyBrowserType = () => {
+  if ('BeforeInstallPromptEvent' in window) {
+  //  console.log("It is a Chromium Browser")
+  isChromiumBrowser.value = true
+  } else {
+    // console.log("It is not a Chromium Browser")
+    isChromiumBrowser.value = false
+
+  }
 }
 
-const showResult = (text, append = false) => {
-  if (append) {
-    outputText.value += "\n" + text;
-  } else {
-    outputText.value = text;
-  }
-};
 
+// Make api call here
+const installAnalytics = () => {
+
+}
+
+// Track installation here for non-chromiun browsers. 
+// This will send analytics the first time the user opens the standalone window
+const trackCustomPwaInstallation = () => {
+  if (window.matchMedia('(display-mode: standalone)').matches && !localStorage.getItem('pwaInstalled') && isChromiumBrowser.value === false) {
+    // Call the analytics function here
+
+    // Test this with an alert function
+    alert("Testing to see if this alert will be called in non chromium browsers standalone display")
+
+    // Set a flag in local storage
+    localStorage.setItem('pwaInstalled', true)
+
+    // Turn isInstalled flag to true to hide button
+    isInstalled.value = true
+  } 
+}
+
+
+// This will run the default PWA installation prompt on button click
 const installApp = async () => {
   if (deferredPrompt.value) {
     deferredPrompt.value.prompt();
-    showResult("🆗 Installation Dialog opened");
-    
+    console.log("🆗 Installation Dialog opened");
     const { outcome } = await deferredPrompt.value.userChoice;
     deferredPrompt.value = null;
-    
     if (outcome === 'accepted') {
-      showResult('😀 User accepted the install prompt.', true);
+      console.log('😀 User accepted the install prompt.');
     } else if (outcome === 'dismissed') {
-      showResult('😟 User dismissed the install prompt');
+      console.log('😟 User dismissed the install prompt');
     }
-    
-    installButtonVisible.value = false;
+    isInstalled.value = true
   }
 };
 
+// This will control the custom PWA installation instructions Modal
+const customPwaInstallation = () => {
+
+}
+
 
 onMounted(() => {
-  if ('BeforeInstallPromptEvent' in window) {
-    showResult("⏳ BeforeInstallPromptEvent supported but not fired yet");
-  } else {
-    showResult("❌ BeforeInstallPromptEvent NOT supported");    
-  }
+  verifyBrowserType();
+
+  trackCustomPwaInstallation()
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt.value = e;
+    console.log(deferredPrompt.value)
+  });
+
+  window.addEventListener('appinstalled', () => {
+    
+  });
 });
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt.value = e;
-  installButtonVisible.value = true;
-  showResult("✅ BeforeInstallPromptEvent fired", true);
-});
 
-window.addEventListener('appinstalled', () => {
-  showResult("✅ AppInstalled fired", true);
-});
 </script>
 
 <template>
@@ -73,11 +107,17 @@ window.addEventListener('appinstalled', () => {
 
       <button 
       @click="installApp" 
-      v-show="installButtonVisible"
-      id="install"
+      v-if="isChromiumBrowser && !isInstalled"
     >
-      Install App
+      Default Install App
     </button>
+    <button 
+    @click="installApp" 
+    v-if="isChromiumBrowser === false && !isInstalled"
+  >
+    Custom Install App
+  </button>
+  
     <output v-html="outputText"></output>
 
     <div>
